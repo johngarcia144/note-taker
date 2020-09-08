c131e2cbf6dc86cf73b6e78126cdedfb6918e533
John Garcia <john.garcia144@gmail.com>
2020-09-08 15:48:47 -0600

    added apt/html routes

---
+++ app/routing/api-routes.js
@@ -0,0 +1,52 @@
+const fs = require("fs")
+const DB_PATH = "../db/db.json"
+const db = require(DB_PATH) || []
+
+
+module.exports = function(app) {
+
+app.get("/api/notes", (req, res) => {
+    res.json(db)
+})
+
+app.post("/api/notes", (req, res) => {
+    const { title, text } = req.body;
+
+    // Handle note validation
+    if (!title || !text) {
+        res.status(400).json({ message: "Please provide a title and a text" });
+        return;
+    }
+
+    // Save note to database
+    const newNote = { title, text }
+    
+    db.push(newNote)
+
+    fs.writeFileSync(DB_PATH, JSON.stringify(db))
+
+    // End client request
+    res.json(newNote)
+
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+    const noteIndex = parseInt(req.params.id);
+
+    // Handle note ID validation
+    if (noteIndex < 0 || noteIndex >= db.length) {
+        res.status(400).json({ message: "Please provide an ID of an existing note" });
+        return;
+    }
+
+    // Delete note from database
+    db.splice(noteIndex, 1);
+
+    fs.writeFileSync(DB_PATH, JSON.stringify(db))
+
+    // End client request
+    res.json({})
+
+});
+
+}
---
+++ app/server.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const path = require('path');
+const fs = require("fs");
+const crypto = require("crypto");
+const util = require("util") // DO I NEED 
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 9080;
+
+// Sets up the Express app to handle data parsing and to read static files
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
